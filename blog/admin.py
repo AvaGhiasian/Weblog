9ed138a955e7777cf75fe3c9a58e31c243539db1
Ava Ghiasian <avaghiasian82@gmail.com>
2025-05-11 21:58:09 +0330

    added image and comment inline

--- blog/admin.py
+++ blog/admin.py
@@ -11,6 +11,17 @@ admin.sites.AdminSite.index_title = "پنل مدیریت"
 
 # Register your models here.
 
+# Inlines
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 0
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'category', 'publish', 'status']
@@ -21,9 +32,9 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     prepopulated_fields = {"slug": ['title']}  # auto generating slug
     list_editable = ['status']
+    inlines = [ImageInline, CommentInline]
     # list_display_links = ['author']
     # autocomplete_fields = ['author']
-    # inlines = [ImageInline, CommentInline]
 
 
 @admin.register(Ticket)
